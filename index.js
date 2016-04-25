c81a58436d500314d40d99485a207917fc09dd3e
Felix Zapata <fzberlinches@gmail.com>
2016-04-25 09:44:19 +0200

    feat(index.js): create skeleton based on the code of the Grunt plugin

--- index.js
+++ index.js
@@ -11,8 +11,9 @@
 var fs = require('fs');
 var path = require('path');
 var R = require('ramda');
+var chalk = require('chalk');
 var through = require('through2');
-var shell = require( 'shelljs' );
+var shell = require('shelljs');
 var PLUGIN_NAME = 'gulp-potomo';
 
 
@@ -24,20 +25,20 @@ function fileExists(filePath) {
   }
 }
 
-function gulppotomo(customOptions, cb) {
+function gulpPotomo(customOptions, cb) {
 
   var defaultOptions = {
-			poDel: false
+    poDel: false
   };
 
   var options = customOptions ? R.merge(defaultOptions, customOptions) : defaultOptions;
-  
+
   function bufferContents(file, enc, cb) {
     
     /* jshint validthis: true */
-    
+
     // Return warning if not found msgfmt command
-		if ( ! shell.which( 'msgfmt' ) ) {
+    if (!shell.which('msgfmt')) {
       this.emit('error', new PluginError(PLUGIN_NAME, 'GNU gettext is not installed in your system PATH.'));
       cb();
       return;
@@ -54,10 +55,39 @@ function gulppotomo(customOptions, cb) {
       return;
     }
 
+    
+    if(!fileExists(file.path)){
+      this.emit('error', new PluginError(PLUGIN_NAME, 'Source file "' + file.path + '" not found.'));
+      cb();
+      return;
+    }
+    
+    // Run external tool synchronously.
+    var command = 'msgfmt -o ' + file.dest + ' ' + file.src[0];
+    if (shell.exec(command).code !== 0) {
+      console.log(chalk.red('Failed to Compile "*.po" files into binary "*.mo" files with "msgfmt".'));
+      shell.exit(1);
+    } else {
+      console.log('File ' + chalk.cyan(file.dest) + ' Created.');
+    }
+
+    // Delete Source PO file(s).
+    if (options.poDel && fileExists(file.src[0])) {
+      fs.removeSync(file.src[0]);
+    }
+
+		// Process the Message.
+		if (this.files.length > 1) {
+      var message = "Total compiled " + this.files.length + ' ".mo" files.';
+      if (options.poDel) {
+        message = "Total compiled " + this.files.length + " and deleted " + this.files.length + ' ".po" files.';
+      }
+      console.log(chalk.green(message));
+    }
+   
     this.push(file);
 
     cb();
-
 
   }
 
@@ -66,4 +96,4 @@ function gulppotomo(customOptions, cb) {
 }
 
 // Exporting the plugin main function
-module.exports = gulppotomo;
+module.exports = gulpPotomo;
