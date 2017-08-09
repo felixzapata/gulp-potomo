447473367ff9fa69306c45a218118b3b76ec577c
Gary Jones <gary@gamajo.com>
2017-08-09 22:25:49 +0100

    CS: Tidy whitespace in test.js

--- test/test.js
+++ test/test.js
@@ -12,7 +12,6 @@ require('mocha');
 
 var fixtures = function(glob) { return path.join(__dirname, './fixtures', glob); }
 
-
 describe('gulp-potomo', function() {
 
   var tmpFolder = path.join(__dirname, 'temp');
@@ -21,7 +20,6 @@ describe('gulp-potomo', function() {
   afterEach(function(done) {
     fs.remove(tmpFolder, done);
   });
-
 
   it('1) Should compile PO to MO with msgfmt.', function(done) {
 
@@ -35,8 +33,6 @@ describe('gulp-potomo', function() {
         actual.should.equal(expected);
         done();
       }));
-
-
   });
 
   it('2) Should compile PO to MO with msgfmt.', function(done) {
@@ -55,40 +51,38 @@ describe('gulp-potomo', function() {
 
   it('3) Should emit error on streamed file', function (done) {
 
-      gulp.src(fixtures('*'), { buffer: false })
-        .pipe(potomo())
-        .on('error', function (err) {
-          err.message.should.eql('Streaming not supported');
-          done();
-       });
-   });
+    gulp.src(fixtures('*'), { buffer: false })
+      .pipe(potomo())
+      .on('error', function (err) {
+        err.message.should.eql('Streaming not supported');
+        done();
+      });
+  });
 
-   xit('4) Should emit error when file is not found', function (done) {
+  xit('4) Should emit error when file is not found', function (done) {
 
-      var missing = path.join(__dirname, './fixtures', 'foobar.po');
-      console.log(missing);
-      gulp.src(missing)
-        .pipe(potomo())
-        .on('error', function (err) {
-          err.message.should.eql('Source file ' + missing + ' not found.');
-          done();
-       });
-   });
+    var missing = path.join(__dirname, './fixtures', 'foobar.po');
+    console.log(missing);
+    gulp.src(missing)
+      .pipe(potomo())
+      .on('error', function (err) {
+        err.message.should.eql('Source file ' + missing + ' not found.');
+        done();
+      });
+  });
 
-    it('5) Should compile PO to MO with msgfmt when path has a space.', function(done) {
+  it('5) Should compile PO to MO with msgfmt when path has a space.', function(done) {
 
-        gulp.src(path.join(__dirname, './fixtures/directory with spaces', 'en_GB.po'))
-            .pipe(potomo())
-            .pipe(sassert.length(1))
-            .pipe(gulp.dest(tmpFolder))
-            .pipe(sassert.end(function(){
-                var expected = fs.readFileSync(path.join(__dirname,'expected/en_GB.mo')).toString();
-                var actual = fs.readFileSync(path.join(__dirname, 'temp/en_GB.mo')).toString();
-                actual.should.equal(expected);
-                done();
-            }));
-
-
-    });
+    gulp.src(path.join(__dirname, './fixtures/directory with spaces', 'en_GB.po'))
+      .pipe(potomo())
+      .pipe(sassert.length(1))
+      .pipe(gulp.dest(tmpFolder))
+      .pipe(sassert.end(function(){
+        var expected = fs.readFileSync(path.join(__dirname,'expected/en_GB.mo')).toString();
+        var actual = fs.readFileSync(path.join(__dirname, 'temp/en_GB.mo')).toString();
+        actual.should.equal(expected);
+        done();
+      }));
+  });
 
 });
